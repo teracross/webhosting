387d6d9b18d6a3e3a0933837eb422e1de26b7a03
teracross <edl@utexas.edu>
2024-09-13 17:26:40 -0700

    Fortmatting changes to Terraform config file

--- deployment/aws/terraform/main.tf
+++ deployment/aws/terraform/main.tf
@@ -10,64 +10,64 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_s3_bucket" "webhosting" {
-    bucket = "eddevhosting"
+  bucket = "eddevhosting"
 
-    tags = {
-        Name = "webhosting"
-        Environment = "Dev"
-    }
+  tags = {
+    Name        = "webhosting"
+    Environment = "Dev"
+  }
 }
 
 resource "aws_s3_object" "index_page" {
-    bucket = aws_s3_bucket.webhosting.id
-    key = "index.html"
-    source = "../../../index.html"
-    etag = md5("../../../index.html")
-    content_type = "text/html"
+  bucket       = aws_s3_bucket.webhosting.id
+  key          = "index.html"
+  source       = "../../../index.html"
+  etag         = md5("../../../index.html")
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "css_sheet" {
-    bucket = aws_s3_bucket.webhosting.id
-    key = "/css/styles.css"
-    source = "../../../css/styles.css"
-    etag = md5("../../../css/syltes.css")
-    content_type = "text/css"
+  bucket       = aws_s3_bucket.webhosting.id
+  key          = "/css/styles.css"
+  source       = "../../../css/styles.css"
+  etag         = md5("../../../css/syltes.css")
+  content_type = "text/css"
 }
 
 resource "aws_s3_bucket_website_configuration" "webhosting_config" {
-    bucket = aws_s3_bucket.webhosting.id
-    
-    index_document {
-      suffix = "index.html"
-    }
+  bucket = aws_s3_bucket.webhosting.id
+
+  index_document {
+    suffix = "index.html"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
-    bucket = aws_s3_bucket.webhosting.id
-    versioning_configuration {
-      status = "Enabled"
-    }
+  bucket = aws_s3_bucket.webhosting.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-    bucket = aws_s3_bucket.webhosting.id
-    policy = data.aws_iam_policy_document.allow_public_access_policy.json
+  bucket = aws_s3_bucket.webhosting.id
+  policy = data.aws_iam_policy_document.allow_public_access_policy.json
 }
 
 data "aws_iam_policy_document" "allow_public_access_policy" {
-    statement {
-        sid = "PublicReadObject"
+  statement {
+    sid = "PublicReadObject"
 
-        actions = [
-            "s3:GetObject"
-        ]
+    actions = [
+      "s3:GetObject"
+    ]
 
-        resources = [
-            "arn:aws:s3:::&{webhosting.name}/*"
-        ]
-    }
+    resources = [
+      "arn:aws:s3:::&{webhosting.name}/*"
+    ]
+  }
 }
